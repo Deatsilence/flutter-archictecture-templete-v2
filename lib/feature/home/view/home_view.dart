28e944622c2d0cedcaf5556e7e6e7f8e2cb8b79a
Deatsilence <mert_im2000@hotmail.com>
2023-12-10 01:43:01 +0300

    auto route added

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -2,14 +2,17 @@ import 'package:architecture_templete_v2/feature/home/view/mixin/home_view_mixin
 import 'package:architecture_templete_v2/product/init/config/app_environment.dart';
 import 'package:architecture_templete_v2/product/init/language/locale_keys.g.dart';
 import 'package:architecture_templete_v2/product/init/product_localization.dart';
+import 'package:architecture_templete_v2/product/navigation/app_router.dart';
 import 'package:architecture_templete_v2/product/utility/constants/enums/locales.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:gen/gen.dart';
 
 part 'widget/home_app_bar.dart';
 
-class HomeView extends StatefulWidget {
+@RoutePage()
+final class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
@@ -33,7 +36,11 @@ class _HomeViewState extends State<HomeView> with HomeViewMixin {
               width: 50,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                final response = await context.router.push<bool?>(
+                  HomeDetailRoute(id: '1'),
+                );
+              },
               child: Text(AppEnvironmentItems.apiKey.value),
             ),
             const Text(LocaleKeys.general_dialog_version_VERSION).tr(),
