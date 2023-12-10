1c2eb66653f1c39de5322fb85070efc8ff982b30
Deatsilence <mert_im2000@hotmail.com>
2023-12-10 11:43:11 +0300

    Custom CachedNetworkImage added

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -4,7 +4,9 @@ import 'package:architecture_templete_v2/product/init/language/locale_keys.g.dar
 import 'package:architecture_templete_v2/product/init/product_localization.dart';
 import 'package:architecture_templete_v2/product/navigation/app_router.dart';
 import 'package:architecture_templete_v2/product/utility/constants/enums/locales.dart';
+import 'package:architecture_templete_v2/product/widget/network_image.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:common/common.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:gen/gen.dart';
@@ -24,51 +26,57 @@ class _HomeViewState extends State<HomeView> with HomeViewMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const _HomeAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Assets.icons.icDoc.svg(package: 'gen'),
-            Assets.lottie.animLoading.lottie(package: 'gen'),
-            Assets.images.imgRocketLunch.image(
-              package: 'gen',
-              height: 50,
-              width: 50,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final response = await context.router.push<bool?>(
-                  HomeDetailRoute(id: '1'),
-                );
-              },
-              child: Text(AppEnvironmentItems.apiKey.value),
-            ),
-            const Text(LocaleKeys.general_dialog_version_VERSION).tr(),
-            const Text(LocaleKeys.general_dialog_version_CONTENT).tr(),
-            const Text(LocaleKeys.general_button_SAVE).tr(args: ['Mert']),
-            OutlinedButton(
-              onPressed: () {
-                ProductLocalization.updateLanguage(
-                  context: context,
-                  value: Locales.en,
-                );
-              },
-              child: const Text(
-                LocaleKeys.general_button_CHANGE_LANGUAGE_ENGLISH,
-              ).tr(),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                ProductLocalization.updateLanguage(
-                  context: context,
-                  value: Locales.tr,
-                );
-              },
-              child: const Text(
-                LocaleKeys.general_button_CHANGE_LANGUAGE_TURKISH,
-              ).tr(),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const ProjectNetworkImage(url: 'https://picsum.photos/200'),
+              const CustomNetworkImage(
+                imageUrl: 'https://picsum.photos/200',
+              ),
+              Assets.icons.icDoc.svg(package: 'gen'),
+              Assets.lottie.animLoading.lottie(package: 'gen'),
+              Assets.images.imgRocketLunch.image(
+                package: 'gen',
+                height: 50,
+                width: 50,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final response = await context.router.push<bool?>(
+                    HomeDetailRoute(id: '1'),
+                  );
+                },
+                child: Text(AppEnvironmentItems.apiKey.value),
+              ),
+              const Text(LocaleKeys.general_dialog_version_VERSION).tr(),
+              const Text(LocaleKeys.general_dialog_version_CONTENT).tr(),
+              const Text(LocaleKeys.general_button_SAVE).tr(args: ['Mert']),
+              OutlinedButton(
+                onPressed: () {
+                  ProductLocalization.updateLanguage(
+                    context: context,
+                    value: Locales.en,
+                  );
+                },
+                child: const Text(
+                  LocaleKeys.general_button_CHANGE_LANGUAGE_ENGLISH,
+                ).tr(),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  ProductLocalization.updateLanguage(
+                    context: context,
+                    value: Locales.tr,
+                  );
+                },
+                child: const Text(
+                  LocaleKeys.general_button_CHANGE_LANGUAGE_TURKISH,
+                ).tr(),
+              ),
+            ],
+          ),
         ),
       ),
     );
